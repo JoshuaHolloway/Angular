60f1211b212715d33dc474026cdd0b24a176de3b
JoshuaHolloway <jhollow6@asu.edu>
2020-02-04 14:25:35 -0600

    Component selector

---
+++ 2/06/app/src/app/servers/servers.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  //selector: 'app-servers', // standard-selector syntax
+  //selector: '[app-servers]', // attribute-selector syntax
+  selector: '.app-servers', // class-selector syntax
+  // templateUrl: './servers.component.html',
+  template: `
+    <app-server></app-server>
+    <app-server></app-server>
+    `,
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

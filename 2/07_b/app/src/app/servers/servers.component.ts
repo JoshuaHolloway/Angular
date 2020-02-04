a4d854cbb9a07617caa47b34d449595b167b36a5
JoshuaHolloway <jhollow6@asu.edu>
2020-02-04 15:18:31 -0600

    Data Binding: Property Binding

---
+++ 2/07_b/app/src/app/servers/servers.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  //selector: 'app-servers', // standard-selector syntax
+  //selector: '[app-servers]', // attribute-selector syntax
+  selector: '.app-servers', // class-selector syntax
+  templateUrl: './servers.component.html',
+  // template: `
+  //   <app-server></app-server>
+  //   `,
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  allowNewServer = false;
+
+  constructor() {
+    const ms = 2e3;
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, ms);
+  }
+
+  ngOnInit() {
+  }
+
+}

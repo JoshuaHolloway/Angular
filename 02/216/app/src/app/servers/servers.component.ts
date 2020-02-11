880aa249493061531b898f138bd7962f675c0558
JoshuaHolloway <jhollow6@asu.edu>
2020-02-10 22:00:47 -0600

    Log all button clicks in an array and output that array below the paragraph. Use a timestamp as the entries into the array.

--- 02/216/app/src/app/servers/servers.component.ts
+++ 02/216/app/src/app/servers/servers.component.ts
@@ -12,21 +12,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
 
-  allowNewServer: boolean = false;
-  serverCreationStatus: string = 'No server was created';
+  private allowNewServer: boolean = false;
+  private serverCreationStatus: string = 'No server was created';
 
   // [09]: Data Starts here and is passed into input in html via [(ngModel)]="serverName"
-  serverName: string = 'initial-server-name';
+  private serverName: string = 'initial-server-name';
 
   // [011]
-  serverCreated = false;
+  private serverCreated: boolean = false;
 
   // [215]
-  servers = ['Testserver', 'Testserver 2'];
+  private servers = ['Testserver', 'Testserver 2'];
 
   // [216]
   private secret_password: string = 'tuna';
-  display_secret_password = true;
+  private display_secret_password: boolean = true;
+  // private button_clicks: Array<string>;
+  private button_clicks: Array<string> = [];
+  private button_click: string;
 
   constructor() {
     const ms = 2e3;
@@ -66,6 +69,9 @@ export class ServersComponent implements OnInit {
   // [216]
   toggle_password(): void {
     this.display_secret_password = !this.display_secret_password;
+
+    const time = Date.now().toString();
+    this.button_clicks.push(time);
   }
 
 }
